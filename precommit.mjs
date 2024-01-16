e77f8f66876f366103ab6293a5dbbdc721cf0887
Nicholas Barrow <nbarrow@inspiretmstech.com>
2024-01-16 16:48:06 -0500

    building out

--- precommit.mjs
+++ precommit.mjs
@@ -1,15 +1,16 @@
 import * as util from "util";
-import { readFileSync } from "fs";
-import { exec as _exec } from "child_process";
+import {readFileSync} from "fs";
+import {exec as _exec} from "child_process";
+
 const exec = util.promisify(_exec);
 
 const current = JSON.parse(readFileSync("./package.json").toString()).version;
 
-const { stdout } = await exec("git show HEAD:package.json");
-const { version: head } = JSON.parse(stdout)
+const {stdout} = await exec("git show HEAD:package.json");
+const {version: head} = JSON.parse(stdout);
 
 console.log({
   current, head
-})
+});
 
-if(current === head) throw new Error("VERSION NOT CHANGED")
+if (current === head) throw new Error("VERSION NOT CHANGED");
